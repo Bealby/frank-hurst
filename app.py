e17ce504e9dea3b0b96f794aeac403f41b91d08e
Bealby <mgbealby@gmail.com>
2021-04-15 08:46:23 +0000

    Created app.py functionality

---
+++ app.py
@@ -0,0 +1,22 @@
+# Imports
+
+import os
+from flask import Flask, render_template, request, url_for
+if os.path.exists("env.py"):
+    import env
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+
+# IP and PORT
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=False)
