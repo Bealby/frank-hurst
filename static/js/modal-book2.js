3cc5b114b9aadb502541ab5f7cd6765ab7342f3c
Bealby <mgbealby@gmail.com>
2021-04-19 14:29:53 +0000

    Modal image functionality for book 1/2/3. JS/CSS/HTML/Commments

--- static/js/modal-book2.js
+++ static/js/modal-book2.js
@@ -1,8 +1,20 @@
+// Book 2 - The Chiang Mai Assignment
+
 // Get the modal
 var modal = document.getElementById("myModal");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg");
+var img = document.getElementById("myImg3");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function () {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg4");
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
 img.onclick = function () {
