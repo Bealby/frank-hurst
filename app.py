13190791319e9cd5490917fbd2198c38747ae076
Bealby <mgbealby@gmail.com>
2022-01-04 08:30:27 +0100

    Add environ functionality in app.py

--- app.py
+++ app.py
@@ -138,4 +138,5 @@ def something_wrong(error):
 # IP and PORT
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-            debug='True')
+            port=int(os.environ.get('PORT')),
+            debug='False')
